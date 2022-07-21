19625b03bf38ce5223a79fc990b1c944419031d5
oucfeng <305085123@qq.com>
2022-07-21 17:09:33 +0800

    ch1-05 more attribute

--- src/ch1-Getting-Started/ch1-05-Shaders/main.cpp
+++ src/ch1-Getting-Started/ch1-05-Shaders/main.cpp
@@ -55,17 +55,19 @@ int main()
     // vertex shader
     const char* vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
         "out vec4 vertexColor;\n"
         "void main()\n"
         "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "   vertexColor = vec4(0.5, 0, 0, 1.0);\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "   vertexColor = vec4(aColor, 1.0);\n"
         "}\0";
     
     //fragment shader
     const char* fragmentShaderSource = "#version 330 core\n"
         "out vec4 FragColor;\n"
         "in vec4 vertexColor;\n"
+        "uniform vec4 globalColor;\n" // 在OpenGL程序代码中设定这个变量
         "void main()\n"
         "{\n"
         "   FragColor = vertexColor;\n"
@@ -122,9 +124,10 @@ int main()
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-        0.0f, 0.5f, 0.0f,   // 右上角
-        0.5f, -0.5f, 0.0f,  // 右下角
-        -0.5f, -0.5f, 0.0f, // 左下角
+        // 位置              // 颜色
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
     };
 
     // 绘制不同的顶点数据时，都要进行该配置过程
@@ -144,8 +147,11 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // 1. 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // VAO 解绑
     glBindVertexArray(0);
@@ -163,7 +169,14 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // 当我们渲染一个物体时要使用着色器程序
+        // 更新一个uniform之前你必须先使用程序（调用glUseProgram)，因为它是在当前激活的着色器程序中设置uniform的。
         glUseProgram(shaderProgram);
+
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int colorLocation = glGetUniformLocation(shaderProgram, "globalColor");
+        glUniform4f(colorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
