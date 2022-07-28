c7f7c724f1d7caaecce11c360f7e0c57816bd82b
oucfeng <305085123@qq.com>
2022-07-28 11:10:39 +0800

    ch3-02 Mesh

---
+++ src/ch3-Model-Loading/ch3-02-Mesh/Mesh.cpp
@@ -0,0 +1,76 @@
+#include "Mesh.h"
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+:vertices(vertices), indices(indices), textures(textures)
+{
+	SetupMesh();
+}
+
+void Mesh::SetupMesh()
+{
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	//VBO数据
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+	//属性值
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+	glEnableVertexAttribArray(0);
+
+	//EBO数据
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
+}
+
+void Mesh::Draw(Shader& shader)
+{
+	shader.use();
+
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+	for (int i = 0; i < textures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i);
+
+		auto texture = textures[i];
+		string name = texture.type;
+		string number;
+		if (name == "texture_diffuse")
+		{
+			number = std::to_string(diffuseNr++);
+		}
+		else if (name == "texture_specular")
+		{
+			number = std::to_string(diffuseNr++);
+		}
+
+		shader.set_uniform(("material." + name + number).c_str(), i);
+		glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, &indices[0]);
+	glBindVertexArray(0);
+}
+
+void Mesh::Release()
+{
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &EBO);
+
+	vertices.clear();
+	indices.clear();
+	textures.clear();
+}
