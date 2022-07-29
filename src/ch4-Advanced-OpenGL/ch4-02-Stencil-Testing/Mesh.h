5a90fb5c7f6330238cc11cab4bbaf78e5c797272
oucfeng <305085123@qq.com>
2022-07-29 15:42:11 +0800

    ch4-02 Stencil Test

---
+++ src/ch4-Advanced-OpenGL/ch4-02-Stencil-Testing/Mesh.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
+
+#include "Shader.h"
+using namespace std;
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    string type;
+    string path;
+};
+
+class Mesh {
+public:
+    /*  网格数据  */
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+    vector<Texture> textures;
+
+    /*  函数  */
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+    void Draw(Shader& shader);
+    void Release();
+private:
+    /*  渲染数据  */
+    unsigned int VAO, VBO, EBO;
+    /*  函数  */
+    void SetupMesh();
+};
