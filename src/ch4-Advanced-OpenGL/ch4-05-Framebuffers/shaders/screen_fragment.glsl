0f0632f00474d665839f39bc51b6380fd794d7b3
oucfeng <oucfeng@gmail.com>
2022-07-31 03:11:15 +0800

    ch4-05 Post-processing Kernel effects

--- src/ch4-Advanced-OpenGL/ch4-05-Framebuffers/shaders/screen_fragment.glsl
+++ src/ch4-Advanced-OpenGL/ch4-05-Framebuffers/shaders/screen_fragment.glsl
@@ -5,9 +5,39 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
+const float offset = 1.0 / 300.0;
+
+vec2 offsets[9] = vec2[](
+	vec2(-offset,  offset), // 左上
+    vec2( 0.0f,    offset), // 正上
+    vec2( offset,  offset), // 右上
+    vec2(-offset,  0.0f),   // 左
+    vec2( 0.0f,    0.0f),   // 中
+    vec2( offset,  0.0f),   // 右
+    vec2(-offset, -offset), // 左下
+    vec2( 0.0f,   -offset), // 正下
+    vec2( offset, -offset)  // 右下
+);
+
+float kernal[9] = float[](
+    -1, -1, -1,
+    -1,  9, -1,
+    -1, -1, -1
+);
+
 void main()
-{    
-    FragColor = texture(screenTexture, TexCoords);
-    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
-    FragColor = vec4(average, average, average, 1.0);
+{
+	vec3 sampleTex[9];
+    for (int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords + offsets[i]));
+    }
+
+    vec3 color = vec3(0.0);
+    for (int i = 0; i < 9; i++)
+    {
+        color += sampleTex[i] * kernal[i];
+    }
+
+    FragColor = vec4(color, 1.0f);
 }
