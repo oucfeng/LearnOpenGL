866cd4bb641812d100593cd6449f09bb81b2e802
oucfeng <oucfeng@gmail.com>
2022-10-03 01:30:57 +0800

    ch5-08 Deferred Shading: A larger number of lights

--- src/ch5-Advanced-Lighting/ch5-08-Deferred-Shading/main.cpp
+++ src/ch5-Advanced-Lighting/ch5-08-Deferred-Shading/main.cpp
@@ -282,10 +282,15 @@ int main()
             deferredLightingShader.set_uniform("lights[" + std::to_string(i) + "].Position", lightPositions[i]);
             deferredLightingShader.set_uniform("lights[" + std::to_string(i) + "].Color", lightColors[i]);
             // update attenuation parameters and calculate radius
+            const float constant = 1.0f; // note that we don't send this to the shader, we assume it is always 1.0 (in our case)
             const float linear = 0.7f;
             const float quadratic = 1.8f;
             deferredLightingShader.set_uniform("lights[" + std::to_string(i) + "].Linear", linear);
             deferredLightingShader.set_uniform("lights[" + std::to_string(i) + "].Quadratic", quadratic);
+            // then calculate radius of light volume/sphere
+            const float maxBrightness = std::fmaxf(std::fmaxf(lightColors[i].r, lightColors[i].g), lightColors[i].b);
+            float radius = (-linear + std::sqrt(linear * linear - 4 * quadratic * (constant - (256.0f / 5.0f) * maxBrightness))) / (2.0f * quadratic);
+            deferredLightingShader.set_uniform("lights[" + std::to_string(i) + "].Radius", radius);
         }
         deferredLightingShader.set_uniform("viewPos", camera.Position);
         // finally render quad
